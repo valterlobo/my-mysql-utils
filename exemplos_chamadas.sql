073f09ece4e470bdb1d3d7b13aefcc3ae19b2985
Valter Lobo <valterlobo@gmail.com>
2020-03-07 18:27:12 -0300

    Create exemplos_chamadas.sql

---
+++ exemplos_chamadas.sql
@@ -0,0 +1,8 @@
+
+SET @qtd = 0 ; 
+CALL  verificar_tabela_campo_valor("nome_tbl", "chave_tbl", "168" , @qtd  ) ;  
+SELECT @qtd as quantidade; 
+
+
+SET @list :=  JSON_ARRAY(162 , 168 , 200 , 400 ) ;
+CALL verificar_referencias_tabela_lista_chave("nome_tbl" ,  @list ) ;
