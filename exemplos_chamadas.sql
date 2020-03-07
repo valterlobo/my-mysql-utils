44bce1e7abd686ffb3b8b2b3bc91badd2b5c2dc5
Valter Lobo <valterlobo@gmail.com>
2020-03-07 19:01:25 -0300

    Update exemplos_chamadas.sql

--- exemplos_chamadas.sql
+++ exemplos_chamadas.sql
@@ -3,6 +3,9 @@ SET @qtd = 0 ;
 CALL  verificar_tabela_campo_valor("nome_tbl", "fk_tbl", "168" , @qtd  ) ;  
 SELECT @qtd as quantidade; 
 
-
 SET @list :=  JSON_ARRAY(162 , 168 , 200 , 400 ) ;
+CALL verificar_referencias_tabela_lista_chave("nome_tbl" ,  @list ) ;
+
+-- verifica a quantidade de registros nas tabelas que possuem referências(FK) 
+SELECT JSON_ARRAYAGG((id))  INTO @list  from "nome_tbl";
 CALL verificar_referencias_tabela_lista_chave("nome_tbl" ,  @list ) ;
